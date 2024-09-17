a7f7f29cd807229c59df898b67ce1d8ad6fd8df9
unknown <mrcricoarnold@gmail.com>
2024-09-17 13:27:20 +0300

    First commit

---
+++ Lab03.1/Lab031.cpp
@@ -0,0 +1,47 @@
+// Lab_03_1.cpp
+// < Симодейко Олександр >
+// Лабораторна робота № 3.1
+// Розгалуження, задане формулою: функція однієї змінної.
+// Варіант 13
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	double x, y, A, B;
+    cout << "x = ";  cin >> x;
+
+	A = 2 * abs(x - 5);
+
+    // спосіб 1: розгалуження в скороченій формі
+    if (x < -1) {
+        B = pow(sin(x), 2) / (1 + abs(cos(x)));
+    }
+    if (x >= -1 && x <= 1) {
+        B = pow(cos(x), 2) * (1 / abs(x + 2));
+    }
+    if (x > 1) {
+        B = log(abs(x + 2));
+    }
+    y = A - B;
+
+    cout << endl;
+    cout << "1)y = " << y << endl;
+
+    // спосіб 2: розгалуження в повній формі
+    if (x < -1) {
+        B = pow(sin(x), 2) / (1 + abs(cos(x)));
+    }
+    else if (x >= -1 && x <= 1) {
+        B = pow(cos(x), 2) * (1 / abs(x + 2));
+    }
+    else if (x > 1) {
+        B = log(abs(x + 2));
+    }
+    y = A - B;
+
+    cout << "2)y = " << y << endl;
+    return 0;
+}
